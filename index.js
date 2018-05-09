a9bac2b8a4859ac0f0c80cad3ded5911ef057bca
Toufiq <burningraven06@gmail.com>
2018-05-09 13:49:14 +0600

    setup sockets in backend & fronend

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io');
 
-var server = app.listen(4000, ()=> {
+//App Setup
+var expressServer = app.listen(4000, ()=> {
 	console.log(`Listening on Port: 4000`);
 });
 
+//Static Files
 app.use(express.static('public'));
+
+//Socket Setup
+const io = socket(expressServer);
+
+io.on('connection', (socket) => {
+	console.log('Socket Connection Made');
+})
